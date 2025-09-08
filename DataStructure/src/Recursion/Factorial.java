083b58b312039190ee41a6e73a51d2f7b2d96737
Kaviyarasan <kavigunasekar185@gmail.com>
2025-09-08 20:15:19 +0530

    recursion start

---
+++ DataStructure/src/Recursion/Factorial.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class Factorial {
+	public static void main(String[] args) {
+		int a =5;
+		int res= fact(a);
+		System.out.println(res);
+	}
+	static int fact(int n) {
+		if(n == 1) {
+			return 1;
+		}
+		return n*fact(n-1);
+	}
+}
