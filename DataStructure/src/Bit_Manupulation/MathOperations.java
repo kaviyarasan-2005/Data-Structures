292e5513a93f1e2970f98813b20212dd0a112988
Kaviyarasan <kavigunasekar185@gmail.com>
2025-10-02 22:05:08 +0530

    Adddition using bit operator

--- DataStructure/src/Bit_Manupulation/MathOperations.java
+++ DataStructure/src/Bit_Manupulation/MathOperations.java
@@ -1,12 +1,12 @@
 package Bit_Manupulation;
 
 public class MathOperations {
-	public static void main() {
-		int a =10,b=20;
+	public static void main(String[] args) {
+		int a =15,b=21;
 		while(b!=0) {
 			int c = (a&b);
 			a = a^b;
-			b = c;
+			b = c<<1;
 		}
 		System.out.println("Addition of A and B is "+ a);
 				
