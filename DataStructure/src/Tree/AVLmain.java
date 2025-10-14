e918bed8fb8bd7a66dacf56c3b658e43050661e9
Kaviyarasan <kavigunasekar185@gmail.com>
2025-10-14 22:15:40 +0530

    AVL tree implementation insertion

--- DataStructure/src/Tree/AVLmain.java
+++ DataStructure/src/Tree/AVLmain.java
@@ -2,6 +2,16 @@ package Tree;
 
 public class AVLmain {
 	public static void main(String[] args) {
-		AVLtree avl = new AVLtree(10);
+		AVLtree avl = new AVLtree(50);
+		avl.insert( 20);
+		avl.insert( 60);
+		avl.insert( 10);
+		avl.insert( 30);
+		avl.insert( 40);
+		System.out.println("Tree in level order ");
+		avl.LevelOrderDisplay(avl.root);
+		System.out.println();
+		System.out.println("Height of the Tree in level order ");
+		avl.LevelOrderHeight(avl.root);
 	}
 }
--- DataStructure/src/Tree/AVLtree.java
+++ DataStructure/src/Tree/AVLtree.java
@@ -1,5 +1,5 @@
 package Tree;
-
+import java.util.*;
 public class AVLtree {
 	Node root;
 	class Node {
@@ -16,5 +16,108 @@ public class AVLtree {
 	}
 	AVLtree(int data){
 		root = new Node(data);
+	}
+	public void insert(int data) {
+		root = insert(root,data);
+	}
+	public int getheight(Node root) {
+		if(root == null) {
+			return -1;
+		}
+		return root.height;
+	}
+	private Node insert(Node root, int data) {
+		if(root == null) {
+			return new Node(data);
+		}
+		if(data<root.data) {
+			root.left = insert(root.left,data);
+		}
+		else if(data> root.data) {
+			root.right = insert(root.right,data);
+		}
+		else {
+		return root;
+		}
+		root.height = 1+Math.max(getheight(root.left),getheight(root.right));
+		int BalanceHeight = balance(root);
+//		Left cases
+		if(BalanceHeight > 1) {
+			// LL case
+			if(data < root.left.data) {
+				return RightRotate(root);
+			}
+			// LR cases
+			else if(data > root.left.data) {
+				root.left = LeftRotate(root.left);
+				return RightRotate(root);
+			}
+		}
+		// RightCases
+		else if(BalanceHeight < -1) {
+			// RR case
+			if(data > root.right.data) {
+				return LeftRotate(root);
+			}
+			//RL case
+			else if(data<root.right.data) {
+				root.right = RightRotate(root.right);
+				return LeftRotate(root);
+			}
+		}
+		return root;
+	}
+	private int balance(Node root) {
+		if(root == null) {
+			return -1;
+		}
+		return getheight(root.left) - getheight(root.right);
+	}
+	public void LevelOrderDisplay(Node root) {
+		Queue<Node> qu = new LinkedList<>();
+		qu.add(root);
+		while(!qu.isEmpty()) {
+			Node node = qu.poll();
+			System.out.print(node.data+" ");
+			if(node.left != null) {
+				qu.add(node.left);
+			}
+			if(node.right != null) {
+				qu.add(node.right);
+			}
+		}
+	}
+	public void LevelOrderHeight(Node root){
+		Queue<Node> qu = new LinkedList<>();
+		qu.add(root);
+		while(!qu.isEmpty()) {
+			Node node = qu.poll();
+			System.out.print(node.height+" ");
+			if(node.left != null) {
+				qu.add(node.left);
+			}
+			if(node.right !=null) {
+				qu.add(node.right);
+			}
+				
+		}
+	}
+	private Node RightRotate(Node root) {
+		Node y = root.left;
+		Node t = y.right;
+		y.right = root;
+		root.left = t;
+		root.height = 1+Math.max(getheight(root.left), getheight(root.right));
+		y.height = 1+Math.max(getheight(y.left), getheight(y.right));
+		return y;
+	}
+	private Node LeftRotate(Node root) {
+		Node y = root.right;
+		Node t = y.left;
+		y.left = root;
+		root.right= t;
+		root.height = 1+Math.max(getheight(root.left), getheight(root.right));
+		y.height = 1+Math.max(getheight(y.left), getheight(y.right));
+		return y;
 	}
 }
